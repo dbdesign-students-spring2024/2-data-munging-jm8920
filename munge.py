7dbf83820b23233b9354fde6e2193cd787f0d51c
jm8920 <jm8920@nyu.edu>
2024-02-12 20:16:19 -0500

    tbc

--- munge.py
+++ munge.py
@@ -1,46 +1,45 @@
 # Place code below to do the munging part of this assignment.
 with open('data/GLB.Ts+dSST.txt', 'r') as file:
-    raw_data = file.readlines()
+    data = file.readlines()
 
 cleaned_data = []
 collect_data = False
 
 # Process each line of raw data
-for line in raw_data:
+for line in data:
     # Remove whitespace
     line = line.strip()
 
-    # if a line starts with "Year," it's the header, so start collecting data
+# Test if a line starts with "Year" 
     if line.startswith("Year"):
         collect_data = True
-        header = line  # store the header for later use
+        header = line  
         continue
 
-    # Append the line to cleaned_data
+# Append the line to cleaned_data
     if collect_data and line:
         values = line.split()
-        # check if the first box contains numeric data
+
+
+# Handle the missing data
         if values[0].isdigit():
-            
-        # Handle missing data by replacing them with 'NaN'
             values = ['NaN' if v in ('***', '****') else v for v in values]
-            # convert temperature anomalies to Fahrenheit (excl. the "Year" columns)
+
+# Convert temperature to Fahrenheit
             for i in range(1, len(values) - 1):
                 if values[i] != 'NaN':
-                    values[i] = "{:.1f}".format(float(values[i]) * 1.8 / 100)
+                    values[i] = "{:.1f}".format(float(values[i]) * 0.018)
             cleaned_data.append(values)
 
-csv_filename = "clean_data.csv"
+new_filename = "clean_data.csv"
 
-# open CSV file for writing
-with open(csv_filename, "w") as csv_file:
-    # write the header row with separate cells
+# Write the data into CSV file
+with open(new_filename, "w") as csv_file:
     header_values = header.split()
     csv_file.write(header_values[0] + ",")
     csv_file.write(",".join(header_values[1:-1]) + ",")
     csv_file.write(header_values[-1] + "\n")
 
-    # write cleaned data rows with separate cells
     for row in cleaned_data:
         csv_file.write(row[0] + ",")
         csv_file.write(",".join(row[1:-1]) + ",")
