ccb2242792567ba8c15f3d70c5fca9f28a0efab4
jm8920 <jm8920@nyu.edu>
2024-02-12 22:08:12 -0500

    finish

--- analyze.py
+++ analyze.py
@@ -1,33 +1,31 @@
+# Place code below to do the munging part of this assignment.
+
 import csv
 
-# open/ read  CSV file
-with open('clean_data.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    header = next(csvreader)  # skip header row
+with open('data/clean_data.csv', 'r') as csvfile:
+    data = csv.reader(csvfile)
+    header = next(data) 
 
-    # group data by decade
-    current_decade = 1880
+    decade_start = 1880
     decade_data = []
-    for row in csvreader:
-        year = int(row[0])  # The year is the first column in the CSV
-        if year < current_decade + 10:
+
+    for row in data:
+        year = int(row[0])  
+        if year < decade_start + 10:
             decade_data.append(row)
+            
         else:
-            # get the avg of the temperature anomalies for the decade
             total = 0
             count = 0
             for year_data in decade_data:
-                # loop through monthly temperature anomalies
-                for month_index in range(1, 13):
-                    temp_anomaly = year_data[month_index]
+                for month in range(1, 13):
+                    temp_anomaly = year_data[month]
                     if temp_anomaly != "NaN":
                         total += float(temp_anomaly)
                         count += 1
             avg_anomaly = total / count if count != 0 else "NaN"
             
-            # output avg for the decade
-            print(f"Average temperature anomaly for {current_decade} to {current_decade + 9}: {avg_anomaly:.2f}°F")
-            
-            # and the next decade
-            current_decade += 10
+            # Output
+            print(f"{decade_start} to {decade_start + 9}: {avg_anomaly:.2f}°F")
+            decade_start += 10
             decade_data = [row]
